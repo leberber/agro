eff57ed1c319c85c638a2dcd31843e1389bfd51a
Yazid <ymekhtoub@gmail.com>
2023-09-21 10:22:59 -0400

    started the cart

--- agro/app.py
+++ agro/app.py
@@ -17,13 +17,58 @@ app = Dash(
 shop = dmc.Paper(
     id = id_dict('page_layout_id', 'shop'),
     style = {'display':'block'},
-    className = 'page',
+    className = 'page' ,
     children = [
-        dmc.Text("shop_page", size="md", color ='red'),
-        dmc.Text("shop_page", size="md"),
-        dmc.Text("Dshop_page", size="md"),
-        dmc.Text("Default text", size="md"),
-        dmc.Text("Default text", size="md"),
+        dmc.Grid(
+            # w = '100%',
+            # position = 'apart',
+            justify = 'center',
+            # style ={'border':'2px solid red'},
+            children = [
+                dmc.Col(
+                    span = 2,
+                    # style ={'border':'2px solid red'},
+                    p = 0,
+                    children = [
+                        dmc.Image(src="/assets/Confitures_MATINA_Abricot.png",    width =  '100%', height = '100%' ),
+                    ]
+                ),
+                dmc.Col(
+                    span = 'auto',
+                    # style ={'border':'1px solid green'},
+                    children = [
+                        dmc.Paper(
+                            # style ={'border':'1px solid yellow'},
+                            pos='relative',
+                            children = [
+                                dmc.Text("Confiture Matina Abricot", size="sm"),
+                                dmc.Text("167.24 DA", size="sm", color ='red', pos='absolute', right=0, top = 0),
+                                dmc.Group(
+                                    children = [
+                                        dmc.Stack(
+                                            align="center",
+                                            children = [
+                                                dmc.Text("0", size="md", color ='red'),
+                                                dmc.Text("Quantity", size="xs"),
+
+                                            ]
+                                        ),
+                                        dmc.Stack(
+                                            align="center",
+                                            children = [
+                                                dmc.Text("0", size="md", color ='red'),
+                                                dmc.Text("Total", size="xs"),
+
+                                            ]
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        )
     ]
 )
 
@@ -81,6 +126,27 @@ app.layout = html.Div(
         ) 
     ]
 )
+
+@app.callback(
+    Output({"type": "page_layout_id", "index": ALL}, "style"), 
+    Output({"type": "page_switcher_action", "index": ALL}, "style"),
+    Output({"type": "page_switcher_action_text", "index": ALL}, "style"),
+    Input({"type": "page_switcher_action", "index": ALL}, "n_clicks"),
+    prevent_initial_call=True
+)
+
+def page_switcher(action_click):
+    print(ctx.outputs_list[0])
+    triggered_input = ctx.triggered_id['index']
+    output_list = [i['id']['index'] for i in ctx.outputs_list[0]]
+    print(output_list)
+    number_of_outputs, triggered_input_index = len(output_list), output_list.index(triggered_input)
+    page_output_list = [{'display':'none'}] * number_of_outputs
+    action_item_output_list = [{}] * number_of_outputs
+    page_output_list[triggered_input_index] = {'display':'block'}
+    action_item_output_list[triggered_input_index] = {'color':'rgb(34, 139, 230)'}
+
+    return page_output_list, action_item_output_list, action_item_output_list
 
 
 clientside_callback(
